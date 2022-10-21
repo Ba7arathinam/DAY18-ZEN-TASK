e2ec280bcbc77fc9a6588b3b654de50d2e46dd8e
BALARATHINAM T <109063403+Ba7arathinam@users.noreply.github.com>
2022-10-21 12:03:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var container=document.createElement("div")
+container.setAttribute("class","container");
+var row=document.createElement("div");
+row.setAttribute("class","row");
+row.classList.add("row" ,"m-3")
+container.append(row);
+
+var res=fetch("https://restcountries.com/v2/all")
+res.then((data)=>data.json()).then((data1)=>foo(data1));
+
+function foo(data1){
+    for(var i=0;i<data1.length;i++){
+        row.innerHTML+=`<div class="col-md-4">
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <h3 class="card-title">${data1[i].name}</h3>
+         <img src="${data1[i].flag}" class="card-img-top" alt="...">
+         <div class="card-body">
+         <h5 class="card-title">Capital:${data1[i].capital}</h5>
+         <h5 class="card-title">Region:${data1[i].region}</h5>
+         <h5 class="card-title">Country Code:${data1[i].alpha2Code}</h5>
+        <center> <a href="https://openweathermap.org/" class="btn btn-primary">Check Weather</a> </center>
+         </div>
+       </div>
+         
+          </div>`;
+      document.body.append(container);
+    }
+}
